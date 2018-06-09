4f28b4e2b6908127c4a287dab3d1a74f836267e3
manoz187 <40119037+manoz187@users.noreply.github.com>
2018-06-09 18:42:25 +0530

    Add files via upload

---
+++ firstproject.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+q=time.ctime()
+print(f"Time started at {q} ")
+i=3
+j=0
+while(j<i):
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=VAn5xYpbVR8https://www.youtube.com/watch?v=VAn5xYpbVR8")
+    j=j+1
+print("three times completed ") 
